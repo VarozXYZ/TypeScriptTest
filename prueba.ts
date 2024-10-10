2bf61809009ad55618c97ec04c7d89be9258b366
VarozXYZ <alvaropiro@gmail.com>
2024-10-10 12:43:26 +0200

    Más codigo

--- prueba.ts
+++ prueba.ts
@@ -1,2 +1,5 @@
-let firstName: string = "Dylan"; // type string
-firstName = "Francis"; // attempts to re-assign the value to a different type
+// let firstName: string = "Dylan"; // type string
+// firstName = "Francis"; // attempts to re-assign the value to a different type
+
+let firstName = "Dylan"; 
+firstName = 33; //  Este codigo da error
